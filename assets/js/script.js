f5059f720775eff0f3d99616555f4530d871e395
pangeransaddamhusain <shusainrastafara25@gmail.com>
2023-02-03 19:34:51 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function menuLike() {
+  var element = document.getElementById('menu-like');
+  element.classList.toggle('showMenuLike');
+}
+
+function menuProfile() {
+  var element = document.getElementById('menu-profile');
+  element.classList.toggle('showMenuProfile');
+}
